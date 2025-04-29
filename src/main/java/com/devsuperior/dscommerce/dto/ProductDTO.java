cb8f74578714abb197696320b7dcd23bba7b894e
Marcio Santos <santosu01@gmail.com>
2025-04-29 08:01:38 -0300

    feat: add custom validation errors message

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -1,12 +1,23 @@
 package com.devsuperior.dscommerce.dto;
 
 import com.devsuperior.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
     private Long id;
+
+    @Size(min = 3, max = 80, message = "Nome deve ter de 3 a 80 caracteres.")
+    @NotBlank(message = "Campo obrigatório")
     private String name;
+
+    @Size(min = 10, message = "Descrição deve ter no minimo 10 caractres.")
+    @NotBlank(message = "Descrição é obrigatório.")
     private String description;
+
+    @Positive(message = "O preco deve ser positivo.")
     private Double price;
     private String imgUrl;
 
