a02444fe70b1169bbdc55faef83589e0a435ba98
Marcio Santos <santosu01@gmail.com>
2025-07-20 15:13:03 -0300

    fix: update main application

--- src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
+++ src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
@@ -1,13 +1,28 @@
 package com.devsuperior.dscommerce;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DscommerceApplication {
+public class DscommerceApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscommerceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+	//		System.out.println("ENCODE = " + passwordEncoder.encode(("123456")));
+	//
+	//		boolean isSame = passwordEncoder.matches("123456", "$2a$10$h33BJ/4cLsu8V.GZ.uQVju2x2Pz4Fn7QAdvZbginQGMop2gi8iXFG");
+	//
+	//		System.out.println("is the same " + isSame);
+		System.out.println("Aplicação iniciada.....");
+	}
 }
