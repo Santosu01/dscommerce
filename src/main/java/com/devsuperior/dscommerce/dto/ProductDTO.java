1f4da67f2cf9c25c839036c545ef3ea4f52badad
Marcio Santos <santosu01@gmail.com>
2025-07-21 22:37:52 -0300

    fix: validation

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -1,10 +1,7 @@
 package com.devsuperior.dscommerce.dto;
 
 import com.devsuperior.dscommerce.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +18,7 @@ public class ProductDTO {
     @NotBlank(message = "Descrição é obrigatório.")
     private String description;
 
+    @NotNull(message = "Campo obrigatório.")
     @Positive(message = "O preco deve ser positivo.")
     private Double price;
     private String imgUrl;
